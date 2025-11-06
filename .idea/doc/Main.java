5995c140bd65c14097ba39d36a8c6f9fa9a4be19
cph-dr147 <dana.albidani@gmail.com>
2025-11-06 18:34:11 +0100

    classes made

---
+++ .idea/doc/Main.java
@@ -0,0 +1,52 @@
+class Main{
++ static void main(String[] args)
+}
+
+ class Media
+   {
+   - String Title
+   - ArrayList<String> genre
+   - double Rating
+   + getTitle(): string
+   + getRating(): double
+   }
+
+   class Movie
+   {
+   - int Year
+   + getYear(): int
+   }
+
+   class Serie
+   {
+   - int Season
+   - int Episode
+   + getSeason(): int
+   + getEpisode (): int
+   }
+
+   class StartMenu {
+   - scanner scan
+
+
+   }
+
+   class Mainmenu{
+
+   }
+
+
+ class User
+ {
+ - String userName
+ - String password
+ - int Number
+ - string letter
+  + getUsername(): String
+  + getPassword(): String
+  + setUsername(name: String): void
+  + setPassword(pw: String): void
+  + setUsername(name: String): void
+  + setPassword(pw: String): void
+
+ }
