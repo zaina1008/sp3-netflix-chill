7fd3864b94d91e2afa510582289703d1531b6bf7
cph-dr147 <dana.albidani@gmail.com>
2025-11-07 13:30:32 +0100

    movie setter og getter

--- src/Movie.java
+++ src/Movie.java
@@ -1,7 +1,15 @@
 public class Movie {
 double Duration;
 
+    public Movie(double duration) {
+        Duration = duration;
+    }
+
     public double getDuration() {
         return Duration;
+    }
+
+    public void setDuration(double duration) {
+        Duration = duration;
     }
 }
